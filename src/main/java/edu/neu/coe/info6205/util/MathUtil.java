07c1439833102221c12d2b4bd5e29f5c48977061
Lavie669 <lavie666999@gmail.com>
2021-04-08 02:58:53 +0800

    upload method for infect simulation

--- src/main/java/edu/neu/coe/info6205/util/MathUtil.java
+++ src/main/java/edu/neu/coe/info6205/util/MathUtil.java
@@ -1,6 +1,7 @@
 package edu.neu.coe.info6205.util;
 
 import edu.neu.coe.info6205.Point;
+import umontreal.iro.lecuyer.probdist.NegativeBinomialDist;
 
 import java.util.Random;
 
@@ -13,6 +14,15 @@ public class MathUtil {
     public static double stdGaussian(double sigma, double u) {
         double X = randomGen.nextGaussian();
         return sigma * X + u;
+    }
+
+    public static int getInfectNumberFromNB(float r, float k){
+        double u = 1 - randomGen.nextDouble();
+        //Get p from definition - mean = n(1 - p)/p
+        //mean is r and n is k according to the paper
+        double p = (double) k / (r + k);
+        NegativeBinomialDist nb = new NegativeBinomialDist(k, p);
+        return nb.inverseFInt(u);
     }
 
     public static int randomDisplacement() {
@@ -42,5 +52,9 @@ public class MathUtil {
 
     public static double distance(Point p1, Point p2) {
         return Math.sqrt(Math.pow(p1.getX() - p2.getX(), 2) + Math.pow(p1.getY() - p2.getY(), 2));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getInfectNumberFromNB(3.0f, 0.1f));
     }
 }
